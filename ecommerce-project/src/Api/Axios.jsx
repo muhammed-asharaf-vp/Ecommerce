67b93ee44f7086739a25eb9b40a35fa2fafa1d43
muhammed-asharaf-vp <mohamedasharafvpp@gmail.com>
2025-10-16 15:53:33 +0530

    full work completed

--- ecommerce-project/src/Api/Axios.jsx
+++ ecommerce-project/src/Api/Axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api=axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://dbveloce.onrender.com/',
     headers: {"Content-Type": "application/json"},
 });
 
